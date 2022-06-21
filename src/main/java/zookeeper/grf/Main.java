28e9d446e9fc89e236a65fe04fd6082f20910232
xsalon <luaksz.dubiel3@gmail.com>
2022-06-22 01:36:41 +0200

    comp

---
+++ src/main/java/zookeeper/grf/Main.java
@@ -0,0 +1,29 @@
+package zookeeper.grf;
+
+import org.example.zoo.Executor;
+
+import java.util.Arrays;
+
+public class Main {
+
+    private static String host_port;
+
+    public static void main(String[] args) {
+//        if (args.length < 4) {
+//            System.err
+//                    .println("USAGE: Executor hostPort znode program [args ...]");
+//            System.exit(2);
+//        }
+//        String hostPort = args[0];
+//        String znode = args[1];
+//        String filename = args[2];
+//        String[] exec = new String[args.length - 3];
+//        System.arraycopy(args, 3, exec, 0, exec.length);
+//        try {
+//            System.out.println(Arrays.toString(args));
+//            new Executor(hostPort, znode, filename, exec).run();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+}
