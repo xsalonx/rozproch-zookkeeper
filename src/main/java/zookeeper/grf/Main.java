84f22b856e73ab1e2beef94d388ce5ca27ca2459
xsalon <luaksz.dubiel3@gmail.com>
2022-06-22 08:25:47 +0200

    with watcher cmd execution works

--- src/main/java/zookeeper/grf/Main.java
+++ src/main/java/zookeeper/grf/Main.java
@@ -1,14 +1,112 @@
 package zookeeper.grf;
 
-import org.example.zoo.Executor;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import zookeeper.grf.app.TaskManager;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-    private static String host_port;
+    private static final ArrayList<TaskManager> tasksMan = new ArrayList<>();
 
+    private static String host_port;
+    private static ZooKeeper mainZk;
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String cmd = "";
+        while (!cmd.equals("exit")) {
+            System.out.print("==> ");
+            cmd = scanner.nextLine().trim();
+            String[] cmdAndParams = cmd.split(" +");
+
+
+            try {
+                if ("conn".equals(cmdAndParams[0])) {
+                    setupConnection(cmd, cmdAndParams);
+                } else if ("lst".equals(cmd)) {
+                    listTasks(cmd, cmdAndParams);
+                } else if ("kt".equals(cmdAndParams[0])) {
+                    killTask(cmd, cmdAndParams);
+                } else if ("lsz".equals(cmdAndParams[0])) {
+                    listNodes(cmd, cmdAndParams);
+                } else if ("desc".equals(cmdAndParams[0])) {
+                    descendantsInsight(cmd, cmdAndParams);
+                } else if ("new".equals(cmdAndParams[0])) {
+                    newTask(cmd, cmdAndParams);
+                } else if (!"".equals(cmd)){
+                    throw new RuntimeException("incorrect command");
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    private static void setupConnection(String cmd, String[] cmdAndParams) throws InterruptedException {
+        if (mainZk != null) {
+            mainZk.close();
+        }
+        boolean connected = false;
+        while (!connected) {
+            try {
+                if (cmdAndParams.length == 1) {
+                    host_port = "172.200.202.2:2181";
+                } else {
+                    host_port = cmdAndParams[1];
+                }
+                mainZk = new ZooKeeper(host_port, 3000, System.out::println);
+                connected = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private static void listTasks(String cmd, String[] cmdAndParams) {
+        for (int i = 0; i < tasksMan.size(); i++) {
+            System.out.println(i + " : " + tasksMan.get(i));
+        }
+    }
+    private static void killTask(String cmd, String[] cmdAndParams) {
+        int i = Integer.parseInt(cmdAndParams[1]);
+        tasksMan.get(i).stop();
+    }
+    private static void killAllTasks() {
+        for (int i=tasksMan.size() - 1; i >= 0; i--) {
+            tasksMan.get(i).stop();
+        }
+    }
+    public static void rmTask(TaskManager tm) {
+        tasksMan.remove(tm);
+    }
+    private static void listNodes(String cmd, String[] cmdAndParams)
+            throws InterruptedException, KeeperException {
+        List<String> res = mainZk.getChildren(cmdAndParams[1], false);
+        System.out.println(res);
+    }
+    private static void descendantsInsight(String cmd, String[] cmdAndParams) {
+        throw new RuntimeException("not implemented");
+    }
+
+    private static void newTask(String cmd, String[] cmdAndParams) throws IOException {
+        String[] exec = new String[cmdAndParams.length - 2];
+        System.arraycopy(cmdAndParams, 2, exec, 0, exec.length);
+        TaskManager tm = new TaskManager(host_port, cmdAndParams[1], String.join(" ", exec));
+        tasksMan.add(tm);
+    }
+}
+
+
 //        if (args.length < 4) {
 //            System.err
 //                    .println("USAGE: Executor hostPort znode program [args ...]");
@@ -25,5 +123,3 @@ public class Main {
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
-    }
-}
---
+++ src/main/java/zookeeper/grf/app/TaskManager.java
@@ -0,0 +1,146 @@
+package zookeeper.grf.app;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.example.zoo.Executor;
+import zookeeper.grf.Main;
+
+import java.io.IOException;
+
+public class TaskManager implements Runnable, Watcher, AsyncCallback.StatCallback {
+
+    private String hostPort;
+    private Process child;
+    private ZooKeeper zk;
+    private String exec;
+    private String znode;
+
+    private boolean end;
+
+    private Thread runTh;
+
+
+    public TaskManager(String hostPort, String znode, String exec) throws IOException {
+        this.exec = exec;
+        this.hostPort = hostPort;
+        this.znode = znode;
+        this.end = false;
+        this.zk = new ZooKeeper(hostPort, 3000, this);
+        runTh = new Thread(this);
+        runTh.start();
+    }
+
+    @Override
+    public void run() {
+        zk.exists(znode, true, this, null);
+        try {
+            while (!end) {
+                System.out.println("while loop");
+                synchronized (this) {
+                    wait();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("ending task: " + this);
+    }
+
+    @Override
+    public void processResult(int rc, String path, Object ctx, Stat stat) {
+        boolean exists = false;
+        switch (rc) {
+            case KeeperException.Code.Ok:
+                exists = true;
+                break;
+            case KeeperException.Code.NoNode:
+                break;
+            case KeeperException.Code.SessionExpired:
+            case KeeperException.Code.NoAuth:
+                end = true;
+                return;
+            default:
+                zk.exists(znode, true, this, null);
+                return;
+        }
+        manageChild(exists);
+    }
+
+
+    @Override
+    public void process(WatchedEvent event) {
+        String path = event.getPath();
+        System.out.println(event);
+        if (event.getType() == Event.EventType.None) {
+            switch (event.getState()) {
+                case SyncConnected:
+                    break;
+                case Expired:
+                    end = true;
+                    break;
+            }
+        } else {
+            if (path != null && path.equals(znode)) {
+                zk.exists(znode, true, this, null);
+            }
+        }
+    }
+
+
+    public void manageChild(boolean ex) {
+        if (!ex) {
+            if (child != null) {
+                System.out.println("Killing child");
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                child = null;
+            }
+        } else {
+            if (child != null) {
+                System.out.println("Rerun child");
+
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                child = Runtime.getRuntime().exec(exec);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stop() {
+        try {
+            System.out.println("stopping: " + this);
+            end = true;
+            synchronized (this) {
+                notifyAll();
+            }
+            Main.rmTask(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TaskManager{" +
+                "hostPort='" + hostPort + '\'' +
+                ", child=" + child +
+//                ", zk=" + zk +
+                ", exec='" + exec + '\'' +
+                ", znode='" + znode + '\'' +
+                ", end=" + end +
+                '}';
+    }
+}
